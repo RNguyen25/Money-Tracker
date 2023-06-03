1935441d1428bd036d781859556c9b089ad165a4
Richard Nguyen <112598771+RNguyen25@users.noreply.github.com>
2023-06-03 01:54:10 -0400

    Update plotlyTest.py
    Additional Tests with plotly.

--- plotlyTest.py
+++ plotlyTest.py
@@ -4,24 +4,10 @@ import plotly.graph_objects as go
 # This dataframe has 244 lines, but 4 distinct values for `day`
 cock = ['food','entertainment','random','asdf']
 val = [3, 2, 10, 14]
+title = "what's up"
+colors = ['gold', 'mediumturquoise', 'blue', 'lightgreen']
 
-data=[go.Pie(labels=cock, values=val, textinfo='label+percent', hole=.3)]
+data=[go.Pie(labels=cock, values=val, textinfo='label+percent', hole=.3,title=title)]
 fig = go.Figure(data)
-#fig.show()
-
-categoryDict = {
-  1: "Food & Dining",
-  2: "Entertainment",
-  3: "Health",
-  4: "Groceries",
-  5: "Clothes & Accessories",
-  6: "Misc."
-}
-
-while(True):
-    cat = input("num")
-    try:
-        cat = categoryDict.get(int(cat))
-        break
-    except:
-        print("Please put in a number\n")
+fig.update_traces(textfont_size=14, marker = dict(colors=colors))
+fig.show()
